b8e709e3368e742c1e69820dc434c4316e08481b
Apple <appleeyespackage@gmail.com>
2023-12-14 09:14:42 +0100

    Create display_outside.js

---
+++ display_outside.js
@@ -0,0 +1,13 @@
+// Display a simple message outside a function that says, This is a text outside a function block
+
+function display(){
+  let i = 1;
+  let j = 2;
+  if(i !== j){
+    let sum = i + j;
+  }
+    return sum;
+}
+
+console.log("This is a text outside a function block");
+displayText()
