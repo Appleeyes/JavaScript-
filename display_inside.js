3224c2668a9ae5feb97cbefac4a6442fe605d638
Apple <appleeyespackage@gmail.com>
2023-12-14 09:15:50 +0100

    Update and rename display.js to display_inside.js

--- display_inside.js
+++ display_inside.js
@@ -1,9 +1,5 @@
 // Build a simple function that displays the following.console.log('THis is a text inside a function block'
-// Display a simple message outside a function that says, This is a text outside a function block
 
 function displayText(){
     console.log('THis is a text inside a function block')
 }
-
-console.log("This is a text outside a function block");
-displayText()
